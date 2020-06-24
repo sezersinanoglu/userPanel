b86eb63660b67a381285578a34b02a98190cced2
unknown <sezersinanoglu@gmail.com>
2020-06-24 18:17:08 +0300

    infobox & accordion button eklendi

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+        accordionItemHeader.classList.toggle("active");
+    });
+});
